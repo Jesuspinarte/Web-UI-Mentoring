81983574f0d5e8b09e44d982dcf123e7618514fe
jesuspinarte <jesuspinarte@gmail.com>
2020-01-31 11:39:38 -0500

    Add Okta OAUTH

--- graphql-express-react/src/App.js
+++ graphql-express-react/src/App.js
@@ -3,6 +3,7 @@ import { Button, Container } from 'reactstrap'
 
 import PostViewer from './PostViewer'
 import PostEditor from './PostEditor'
+import withAuth from './withAuth'
 
 class App extends Component {
   state = {
@@ -10,25 +11,48 @@ class App extends Component {
   }
 
   render() {
+    const { auth } = this.props
+    if (auth.loading) return null
+
+    const { user, login, logout } = auth
     const { editing } = this.state
 
     return (
       <Container fluid>
-        <Button
-          className="my-2"
-          color="primary"
-          onClick={() => this.setState({ editing: {} })}
-        >
-          New Post
-        </Button>
+        {user ? (
+          <div>
+            <Button
+              className = 'my-2'
+              color = 'primary'
+              onClick = { () => this.setState({ editing: {} }) }
+            >
+              New Post
+            </Button>
+            <Button
+              className = 'm-2'
+              color = 'secondary'
+              onClick = { () => logout() }
+            >
+              Sign Out (signed in as { user.name })
+            </Button>
+          </div>
+        ) : (
+          <Button
+            className = 'my-2'
+            color = 'primary'
+            onClick = { () => login() }
+          >
+            Sign In
+          </Button>
+        )}
         <PostViewer
-          canEdit={() => true}
-          onEdit={(post) => this.setState({ editing: post })}
+          canEdit = { () => Boolean(user) }
+          onEdit = { (post) => this.setState({ editing: post }) }
         />
         {editing && (
           <PostEditor
-            post={editing}
-            onClose={() => this.setState({ editing: null })}
+            post = { editing }
+            onClose= { () => this.setState({ editing: null }) }
           />
         )}
       </Container>
@@ -36,4 +60,4 @@ class App extends Component {
   }
 }
 
-export default App
+export default withAuth(App)
